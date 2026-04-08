c18369649cf2e1e8458e332fb7aa0775bf0c2b99
paddi-sam <sammypaddison@gmail.com>
2026-04-09 00:08:31 +0100

    chore: tailwind working for real this time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html"],
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.{js,html}",
+    "./**/*.html"
+  ],
   theme: {
     extend: {},
   },
